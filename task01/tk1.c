34c2a0ed8f493bf92dbfe93f6ddc9b569d2f4e42
root <root@li708-47.members.linode.com>
2017-02-23 01:25:41 +0000

    Task one in progress.

---
+++ task01/tk1.c
@@ -0,0 +1,13 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/delay.h>
+
+
